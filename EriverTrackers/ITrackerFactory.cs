6b2a712cc730ae8fea6023d8f455169acf234868
Johan Fogelström <zapmew@gmail.com>
2013-11-18 10:02:58 +0100

    Added ITrackerFactory for future features

---
+++ EriverTrackers/ITrackerFactory.cs
@@ -0,0 +1,14 @@
+﻿using Eriver.Trackers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EriverTrackers
+{
+    interface ITrackerFactory
+    {
+        string getName();
+        ITracker Create();
+    }
+}
